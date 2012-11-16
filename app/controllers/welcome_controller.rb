56a596910a88a26fd0bc3b074a39a51b27a8fd1c
Woon Jin Yong <jinyong91@hotmail.com>
2012-11-16 20:21:22 +0800

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+    # whatever your controller needs to do...
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'd2dc1f09cffc0dd3aa18e89404569154e4963789cf4ad34449832540e2e1d6dfd6e19038ea3b4bf44ab8c19b2c2830491239b6ddeb084fdaa4c3b77ed7442ccc'
